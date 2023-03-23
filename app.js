adc3fac7004cddc5aad0e92632733b241a6749f1
Divhunt <divhunt@divhunt.local.divhunt.com>
2023-03-23 18:34:54 -0400

    Added Accepting URL Header

--- app.js
+++ app.js
@@ -3,19 +3,26 @@ const puppeteer = require('puppeteer');
 
 const app = express();
 
-// Define your routes
 app.get('/', (req, res) =>
 {
+    const url = req.headers?.url;
+
+    if(!url)
+    {
+        res.send('');
+    }
+
     (async () => {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
-        await page.goto('https://divhunt.com', { waitUntil: 'networkidle0' });
+        await page.goto(url, { waitUntil: 'networkidle0' });
         const html = await page.content();
         await browser.close();
         res.send(html);
     })();
 });
 
-app.listen(3000, () => {
+app.listen(3000, () =>
+{
     console.log('Server running on port 3000');
 });
